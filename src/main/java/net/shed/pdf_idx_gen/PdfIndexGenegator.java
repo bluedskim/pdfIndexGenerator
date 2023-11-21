8a1c486d57a40f333258b3c338346cceee6d5d3e
Dennis <bluedskim@gmail.com>
2023-11-21 13:03:44 +0900

    genIdx()에서 PDDocument로 읽어서 바로 반환

--- src/main/java/net/shed/pdf_idx_gen/PdfIndexGenegator.java
+++ src/main/java/net/shed/pdf_idx_gen/PdfIndexGenegator.java
@@ -1,7 +1,10 @@
 package net.shed.pdf_idx_gen;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.io.File;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +12,17 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class PdfIndexGenegator {
-	public Path genIdx(Path orgPdfPath) {
-		return orgPdfPath;
+	public Path genIdx(Path orgPdfPath) throws IOException {
+		log.info("converting orgPdfPath={}" + orgPdfPath);
+		PDDocument orgPdf = PDDocument.load(orgPdfPath.toFile());
+		PDDocument indexedPdf = orgPdf;
+		File indexedPdfFile = new File(
+			orgPdfPath.getParent().toString()
+			+ File.separator
+			+ orgPdfPath.toFile().getName()
+			+ "_indexed"
+		);
+		indexedPdf.save(indexedPdfFile);
+		return indexedPdfFile.toPath();
 	}
 }
