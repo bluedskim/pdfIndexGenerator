2ffc4dee8396ef00947bdbb6f09c9b1c430e69b0
Dennis <bluedskim@gmail.com>
2023-11-21 10:30:15 +0900

    import spring boot prj

---
+++ src/main/java/net/shed/pdf_idx_gen/PdfIndexGeneratorApplication.java
@@ -0,0 +1,13 @@
+package net.shed.pdf_idx_gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfIndexGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfIndexGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/shed/pdf_idx_gen/PdfIndexGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shed.pdf_idx_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfIndexGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
