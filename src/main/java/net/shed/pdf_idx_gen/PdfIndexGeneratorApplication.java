aad98460b13382fb913406adc342fd43e6ecaa95
Dennis <bluedskim@gmail.com>
2023-11-21 10:55:01 +0900

    add log for testing reload

--- src/main/java/net/shed/pdf_idx_gen/PdfIndexGeneratorApplication.java
+++ src/main/java/net/shed/pdf_idx_gen/PdfIndexGeneratorApplication.java
@@ -3,11 +3,13 @@ package net.shed.pdf_idx_gen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PdfIndexGeneratorApplication {
+import lombok.extern.slf4j.Slf4j;
 
+@SpringBootApplication
+@Slf4j
+public class PdfIndexGeneratorApplication {
 	public static void main(String[] args) {
+		log.info("##### starting...");
 		SpringApplication.run(PdfIndexGeneratorApplication.class, args);
 	}
-
 }
