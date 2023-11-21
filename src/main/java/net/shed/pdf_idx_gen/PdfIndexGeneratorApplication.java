1938630dae2453be37c513103570044034b8a077
Dennis <bluedskim@gmail.com>
2023-11-21 11:34:10 +0900

    추가 : genIdx()

--- src/main/java/net/shed/pdf_idx_gen/PdfIndexGeneratorApplication.java
+++ src/main/java/net/shed/pdf_idx_gen/PdfIndexGeneratorApplication.java
@@ -2,14 +2,24 @@ package net.shed.pdf_idx_gen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@RestController
 @Slf4j
 public class PdfIndexGeneratorApplication {
+	@PostMapping(value="/genIdx")
+	String index(@RequestParam("orgPdf") MultipartFile orgPdf) {
+		log.info("orgPdf={}", orgPdf.getOriginalFilename());
+		return orgPdf.getOriginalFilename();
+	}
+
 	public static void main(String[] args) {
-		log.info("##### starting...");
 		SpringApplication.run(PdfIndexGeneratorApplication.class, args);
 	}
 }
