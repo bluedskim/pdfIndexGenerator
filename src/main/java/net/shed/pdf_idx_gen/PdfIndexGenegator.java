a4d368dc77c82e3b525edebbb10168c2dfdfc478
Dennis <bluedskim@gmail.com>
2023-11-21 13:37:41 +0900

    원본에서 페이지를 순서대로 읽어서 그대로 저장

--- src/main/java/net/shed/pdf_idx_gen/PdfIndexGenegator.java
+++ src/main/java/net/shed/pdf_idx_gen/PdfIndexGenegator.java
@@ -14,15 +14,19 @@ import lombok.extern.slf4j.Slf4j;
 public class PdfIndexGenegator {
 	public Path genIdx(Path orgPdfPath) throws IOException {
 		log.info("converting orgPdfPath={}" + orgPdfPath);
-		PDDocument orgPdf = PDDocument.load(orgPdfPath.toFile());
-		PDDocument indexedPdf = orgPdf;
 		File indexedPdfFile = new File(
 			orgPdfPath.getParent().toString()
 			+ File.separator
 			+ orgPdfPath.toFile().getName()
 			+ "_indexed"
 		);
-		indexedPdf.save(indexedPdfFile);
+		PDDocument orgPdf = PDDocument.load(orgPdfPath.toFile());
+		try(PDDocument indexedPdf = new PDDocument();) {
+			for(int i = 0 ; i < orgPdf.getNumberOfPages() ; i++) {
+				indexedPdf.addPage(orgPdf.getPage(i));
+			}
+			indexedPdf.save(indexedPdfFile);
+		}
 		return indexedPdfFile.toPath();
 	}
 }
