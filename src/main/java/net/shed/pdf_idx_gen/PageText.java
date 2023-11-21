0c8c9b45492b0b13086b2490d825d6ff247701c6
Dennis <bluedskim@gmail.com>
2023-11-21 17:40:03 +0900

    추가 PageText

---
+++ src/main/java/net/shed/pdf_idx_gen/PageText.java
@@ -0,0 +1,26 @@
+package net.shed.pdf_idx_gen;
+
+import org.apache.pdfbox.pdmodel.PDPage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class PageText implements Comparable<PageText>{
+	private String content;
+	private int pageNo;
+	private PDPage PDPage;
+
+	@Override
+	public int compareTo(PageText arg0) {
+		return content.compareTo(arg0.getContent());
+	}
+
+  @Override
+  public boolean equals(Object arg0) {
+		return content.equals(((PageText)arg0).getContent());
+	}
+}
