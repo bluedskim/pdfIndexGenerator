ed27f05de1ab43176b3f919e8918409c2d8ffd8c
Dennis <bluedskim@gmail.com>
2023-11-21 17:57:57 +0900

    추가 : 정렬

--- src/main/java/net/shed/pdf_idx_gen/PdfIndexGenegator.java
+++ src/main/java/net/shed/pdf_idx_gen/PdfIndexGenegator.java
@@ -1,8 +1,11 @@
 package net.shed.pdf_idx_gen;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -23,16 +26,25 @@ public class PdfIndexGenegator {
 		);
 		PDDocument orgPdf = PDDocument.load(orgPdfPath.toFile());
 		PDFTextStripper reader = new PDFTextStripper();
+		List<PageText> pageTexts = new ArrayList<>();
+		for(int i = 0 ; i < orgPdf.getNumberOfPages() ; i++) {
+			reader.setStartPage(i);
+			reader.setEndPage(i);
+			String pageText = reader.getText(orgPdf);
+			log.info(pageText);
+			pageTexts.add(new PageText(pageText, i, orgPdf.getPage(i)));
+		}
+
+		Collections.sort(pageTexts);
+
 		try(PDDocument indexedPdf = new PDDocument();) {
-			for(int i = 0 ; i < orgPdf.getNumberOfPages() ; i++) {
-				reader.setStartPage(i);
-				reader.setEndPage(i);
-				String pageText = reader.getText(orgPdf);
-				log.info(pageText);
-				indexedPdf.addPage(orgPdf.getPage(i));
+			for(PageText pageText : pageTexts ) {
+				log.info(pageText.getContent());
+				indexedPdf.addPage(pageText.getPDPage());
 			}
 			indexedPdf.save(indexedPdfFile);
 		}
+
 		return indexedPdfFile.toPath();
 	}
 }
